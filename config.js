5e6a2e1ff7f7d4f3a61fa8647ba31d7d3ce339fc
ayanali7860 <mptlu75@gmail.com>
2026-05-01 23:23:25 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/EWd7TIsEYMiDZZVdtkU0pA',
+    ADMIN_LIST_PATH: './admin.json',
+    IMAGE_PATH: 'https://files.catbox.moe/otbham.jpg', //ඒක හරි 
+    NEWSLETTER_JID: '120363425072775595@newsletter',
+    NEWSLETTER_MESSAGE_ID: '428',
+    OTP_EXPIRY: 300000,
+    NEWS_JSON_URL: '',
+    BOT_NAME: 'DCT MINI BOT',
+    OWNER_NAME: 'DCT MD BOT',
+    OWNER_NUMBER: '94721164497',
+    BOT_VERSION: '1.0.0',
+    BOT_FOOTER: '> 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐖𝐚𝐪𝐚𝐫 𝐌𝐢𝐧𝐢 ✨',
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb6qeR8HVvTbWqv2hh1y',
+};
